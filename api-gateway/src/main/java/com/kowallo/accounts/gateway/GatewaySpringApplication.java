3b92b25207dc15ba4f757cb925d483765451611e
Dominik <dkkd3046@gmail.com>
2026-06-02 17:18:24 +0200

    Repo init

---
+++ api-gateway/src/main/java/com/kowallo/accounts/gateway/GatewaySpringApplication.java
@@ -0,0 +1,11 @@
+package com.kowallo.accounts.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaySpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaySpringApplication.class, args);
+    }
+}
